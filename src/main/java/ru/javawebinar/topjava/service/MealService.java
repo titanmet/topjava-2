aa57ce9730c441c16edabd683aa831a38f8f1600
titanmet <titanmet@mail.ru>
2019-02-17 20:55:09 +0300

    hw02

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,4 +1,18 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.util.Collection;
+
 public interface MealService {
+    Meal create(Meal meal);
+
+    Meal update(Meal meal);
+
+    void delete(int id) throws NotFoundException;
+
+    Meal get(int id) throws NotFoundException;
+
+    Collection<Meal> getAll();
 }
--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,9 +1,39 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import java.util.Collection;
+
+@Service
 public class MealServiceImpl implements MealService {
 
     private MealRepository repository;
 
+    @Override
+    public Meal create(Meal meal) {
+        return null;
+    }
+
+    @Override
+    public Meal update(Meal meal) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) throws NotFoundException {
+
+    }
+
+    @Override
+    public Meal get(int id) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public Collection<Meal> getAll() {
+        return null;
+    }
 }
