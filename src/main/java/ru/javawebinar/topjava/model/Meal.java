43a708de4062db2944dad46d58ba958c8e25fe58
titanmet <titanmet@mail.ru>
2019-03-01 12:21:18 +0300

    Hibernate entity Meal

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,17 +1,48 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.Range;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQueries({
+        @NamedQuery(name = Meal.UPDATE,query = "UPDATE Meal m SET m.description=:description," +
+                "m.calories=:calories,m.dateTime=:date_time WHERE m.id=:id AND m.user.id=:user_id"),
+        @NamedQuery(name = Meal.DELETE,query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:user_id"),
+        @NamedQuery(name = Meal.GET,query = "SELECT m FROM Meal m WHERE m.id=:id AND m.user.id=:user_id"),
+        @NamedQuery(name = Meal.ALL,query = "SELECT m FROM Meal m WHERE m.user.id=:user_id ORDER BY m.dateTime DESC "),
+        @NamedQuery(name = Meal.BETWEEN,query = "SELECT m FROM Meal m WHERE m.user.id=:user_id " +
+                "AND (m.dateTime BETWEEN :startDate AND :endDate) ORDER BY m.dateTime DESC ")
+})
+
+@Entity
+@Table(name = "meals",uniqueConstraints = {@UniqueConstraint(columnNames = "user_id",name="meals_unique_user_datetime_idx"),
+        @UniqueConstraint(columnNames = "date_time",name="meals_unique_user_datetime_idx")})
 public class Meal extends AbstractBaseEntity {
+    public static final String UPDATE="Meal.update";
+    public static final String DELETE="Meal.delete";
+    public static final String GET="Meal.get";
+    public static final String ALL="Meal.getAll";
+    public static final String BETWEEN="Meal.getBetween";
+
+    @Column(name = "date_time", nullable = false,unique = true)
+    @NotNull
+    @DateTimeFormat
     private LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
+    @NotBlank
     private String description;
 
+    @Column(name = "calories", nullable = false)
+    @Range(min = 10,max = 10000)
     private int calories;
+
 
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
